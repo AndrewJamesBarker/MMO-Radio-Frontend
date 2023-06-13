cf37fc0332c45aba752694a0b7513e47e3391d26
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-06-12 22:51:06 -0400

    audio meta data endpoint retrieval, styling

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -1,7 +1,14 @@
 
 import dotenv from 'dotenv';
 import React, { useState, useEffect, useRef } from 'react';
-// retrieve and play audio content from producer back endianness, via icecast
+
+// Retrieve and play audio content from producer back end CMS, via icecast
+
+// Also, retieve data from icecast regarding meta data from the stream. 
+// currently grabbing the amount of active listeners, and the title of what is currently 
+// playing.
+
+// potential url variables from api endpoints (in .env)
 
 // const streamStatsURL = process.env.REACT_APP_STREAM_DATA;
 // const audioStreamURL = process.env.REACT_APP_AUDIO_STREAM;
@@ -11,37 +18,52 @@ import React, { useState, useEffect, useRef } from 'react';
 
 
 const AudioPlayer = () => {
+  const audioUrl = 'http://singhmansingh.games:8000/brickmmo-radio.mp3'
+  const dataUrl = 'http://singhmansingh.games:8000/status-json.xsl'
   const audioRef = useRef(null);
-  const [duration, setDuration] = useState(0);
   const [metadata, setMetadata] = useState(null);
+  const [listenerCount, setListenerCount] = useState(null);
+  const [trackTitle, setTracktitle] = useState(null);
 
   useEffect(() => {
-    // Add event listener for the 'loadedmetadata' event
-    audioRef.current.addEventListener('loadedmetadata', handleLoadedMetadata);
+    fetchAudioMetaData();
+    // fetchPlayerMetaData();
+  }, [trackTitle, listenerCount]);
+  
+  const fetchAudioMetaData = async () => {
+    try {
+      const response = await fetch(dataUrl);
+      const data = await response.json();
+      const listeners = data.icestats.source.listeners;
+      const title = data.icestats.source.title;
 
-    // Clean up the event listener when the component is unmounted
-    return () => {
-      audioRef.current.removeEventListener('loadedmetadata', handleLoadedMetadata);
-    };
-  }, []);
+      setListenerCount(listeners);
+      setTracktitle(title);
 
-  const handleLoadedMetadata = () => {
-    // Access the duration and metadata properties of the audio element
-    setDuration(audioRef.current.duration);
-    setMetadata(audioRef.current.metadata);
-  };
+    } catch (error) {
+      console.error('error fetching audo data', error);
+    }
+  }
+// this function woyld be for retrieving some metadata directly from within the stream itself,
+// not the alternate api endpoint currently in use 
 
+  // const fetchPlayerMetaData = async () => {
+    
+  // }
+  
   return (
     <div className="basic-container player-container">
       <audio ref={audioRef} controls>
-        <source src="http://singhmansingh.games:8000/brickmmo-radio.mp3" type="audio/mpeg" />
+        <source src={audioUrl} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
       <div>
-        {/* Duration: {duration} seconds */}
-      </div>
-      <div>
-        {/* Metadata: {metadata} */}
+      <p>
+        <span className='orange-text'>Now Playing:</span> {trackTitle}
+        </p>
+        <p>
+        <span className='orange-text'>Listener Count:</span> {listenerCount}
+        </p>
       </div>
     </div>
   );
